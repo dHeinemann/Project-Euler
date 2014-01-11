85e413e21b54090d69432902cee28377f5c533d7
David Heinemann <dheinemann@fastmail.fm>
2014-01-11 21:20:13 +1000

    Replaced a loop in euler2.pl with a list comprehension.

--- Perl/euler2.pl
+++ Perl/euler2.pl
@@ -1,17 +1,18 @@
 #!/usr/bin/perl
 use warnings;
 use strict;
+use List::Util qw(sum);
 
-my @fib;
-&calculateFibonacci();
-&calculateSumOfEvens();
+my $sum = sum(grep { $_ % 2 == 0 } &calculateFibonacci(4000000));
+print("Sum: $sum\n");
 
 sub calculateFibonacci()
 {
+    my @fib;
     my $back1 = 1;
     my $back2 = 0;
 
-    for (my $i = 0; $i < 4000000; $i++)
+    for (my $i = 0; $i < $_[0]; $i++)
     {
         if ($i == ($back1 + $back2))
         {
@@ -20,18 +21,6 @@ sub calculateFibonacci()
             $back1 = $i;
         }
     }
-    print("@fib\n");
-}
 
-sub calculateSumOfEvens()
-{
-    my $sum = 0;
-    foreach(@fib)
-    {
-        if ($_ % 2 == 0)
-        {
-            $sum += $_;
-        }
-    }
-    print("Sum: $sum\n");
+    return @fib;
 }
